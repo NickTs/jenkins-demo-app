d1e505d367ba960d33f059ef57d2100ebf8ac598
tsuprick <tsuprick@gmail.com>
2019-06-26 15:07:22 +0300

    my commit auto-deploy 4-17

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello PM2 Nick 16");
+  res.send("Hello PM2 Nick 17");
 });
 
 app.listen(4000);
